3eda7e89fe7feb0dfed9e8f6bbd4f0ab9d3e7f5b
bda82 <bda82@mail.ru>
2022-09-22 21:13:16 +0300

    initial

---
+++ src/main/java/com/mycompany/tracker/Tracker.java
@@ -0,0 +1,12 @@
+package com.mycompany.tracker;
+
+/**
+ *
+ * @author user
+ */
+public class Tracker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
